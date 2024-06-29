1f4b431a74b75a69ad9c9b8a29d064d6f902cf82
joaomstaziaki <joao.m.staziaki@gmail.com>
2024-06-28 21:10:39 -0400

    carousel (1/2)

--- tagilata restaurant/carousel.js
+++ tagilata restaurant/carousel.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 
+    $('.carousel-image').each(function(){
+        var img = new Image();
+        img.src = $(this).attr('src');
+    });
+
     $('.next').on('click', function(){
         var currentImg = $('.active');
+        var leftImg = $('.left');
+        var rightImg = $('.right');
         var nextImg = currentImg.next();
+        var prevImg = currentImg.prev();
 
         if(nextImg.length){
+            
+            
+            prevImg
+            currentImg.addClass('.left').css('z-index', 1);
+
             currentImg.removeClass('active').css('z-index', -10);
             nextImg.addClass('active').css('z-index', 10);
+
+            
+        } else {
+            currentImg.removeClass('active').css('z-index', -10);
+            $('.carousel-image').first().addClass('active').css('z-index', 10);
         }
     });
 
@@ -17,6 +35,9 @@ $(document).ready(function(){
         if(prevImg.length){
             currentImg.removeClass('active').css('z-index', -10);
             prevImg.addClass('active').css('z-index', 10);
+        } else {
+            currentImg.removeClass('active').css('z-index', -10);
+            $('.carousel-image').last().addClass('active').css('z-index', 10);
         }
     });
 
