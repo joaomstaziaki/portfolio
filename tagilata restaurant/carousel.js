60eba298a367e12eec41e6391195207f15823ade
joaomstaziaki <joao.m.staziaki@gmail.com>
2024-07-01 20:25:22 -0400

    carousel basically done, sections, text

--- tagilata restaurant/carousel.js
+++ tagilata restaurant/carousel.js
@@ -1,44 +1,51 @@
 $(document).ready(function(){
 
-    $('.carousel-image').each(function(){
-        var img = new Image();
-        img.src = $(this).attr('src');
+    function rotateNext() {
+        var currentImg = $('.carousel-image.active');
+        var prevImg = currentImg.prev().length ? currentImg.prev() : $('.carousel-image').last();
+        var nextImg = currentImg.next().length ? currentImg.next() : $('.carousel-image').first();
+        var nextnextImg = nextImg.next().length ? nextImg.next() : $('.carousel-image').first();
+
+        prevImg.removeClass('left').css('z-index', -10);
+        currentImg.removeClass('active').css('z-index', -10);
+        nextImg.removeClass('right').css('z-index', -10);
+
+        currentImg.addClass('left').css('z-index', 1);
+        nextImg.addClass('active').css('z-index', 2);
+        nextnextImg.addClass('right').css('z-index', 1);
+    }
+
+    function rotatePrev() {
+        var currentImg = $('.carousel-image.active');
+        var prevImg = currentImg.prev().length ? currentImg.prev() : $('.carousel-image').last();
+        var nextImg = currentImg.next().length ? currentImg.next() : $('.carousel-image').first();
+        var prevprevImg = prevImg.prev().length ? prevImg.prev() : $('.carousel-image').last();
+
+        prevImg.removeClass('left').css('z-index', -10);
+        currentImg.removeClass('active').css('z-index', -10);
+        nextImg.removeClass('right').css('z-index', -10);
+
+        currentImg.addClass('right').css('z-index', 1);
+        prevImg.addClass('active').css('z-index', 2);
+        prevprevImg.addClass('left').css('z-index', 1);
+    }
+
+    var autoRotate = setInterval(rotateNext, 5000);
+
+    function resetTimer() {
+        clearInterval(autoRotate);
+        autoRotate = setInterval(rotateNext, 5000);
+    }
+
+    $('.next').on('click', function(event){
+        event.preventDefault();
+        rotateNext();
+        resetTimer();
     });
 
-    $('.next').on('click', function(){
-        var currentImg = $('.active');
-        var leftImg = $('.left');
-        var rightImg = $('.right');
-        var nextImg = currentImg.next();
-        var prevImg = currentImg.prev();
-
-        if(nextImg.length){
-            
-            
-            prevImg
-            currentImg.addClass('.left').css('z-index', 1);
-
-            currentImg.removeClass('active').css('z-index', -10);
-            nextImg.addClass('active').css('z-index', 10);
-
-            
-        } else {
-            currentImg.removeClass('active').css('z-index', -10);
-            $('.carousel-image').first().addClass('active').css('z-index', 10);
-        }
+    $('.prev').on('click', function(event){
+        event.preventDefault();
+        rotatePrev();
+        resetTimer();
     });
-
-    $('.prev').on('click', function(){
-        var currentImg = $('.active');
-        var prevImg = currentImg.prev();
-
-        if(prevImg.length){
-            currentImg.removeClass('active').css('z-index', -10);
-            prevImg.addClass('active').css('z-index', 10);
-        } else {
-            currentImg.removeClass('active').css('z-index', -10);
-            $('.carousel-image').last().addClass('active').css('z-index', 10);
-        }
-    });
-
 });
